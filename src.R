126845243f5bffb4ec233f6c67b898e3847c4bde
gustavo-tm <gtheilm@gmail.com>
2023-10-26 15:31:25 -0300

    descritiva piramide

--- src.R
+++ src.R
@@ -1,17 +1,41 @@
 library(tidyverse)
 library(readr)
 
-df <- data.table::fread("perfil_comparecimento_abstencao_2022.csv", sep = ";",
+df <- data.table::fread("dados/ignore/perfil_comparecimento_abstencao_2022.csv", sep = ";",
                         encoding = "Latin-1", showProgress = TRUE)
-municipios_tse <- read_csv("municipios_brasileiros_tse.csv")
-passe_livre <- read_csv("passe_livre.csv")
+municipios_tse <- read_csv("dados/municipios_brasileiros_tse.csv")
+passe_livre <- read_csv("dados/passe_livre.csv")
 
-df <- df %>% 
+df <- df_original %>% 
   select(ano = ANO_ELEICAO, turno = NR_TURNO, municipio = CD_MUNICIPIO, 
          faixa_etaria = DS_FAIXA_ETARIA, aptos = QT_APTOS, abstencoes = QT_ABSTENCAO) %>% 
   group_by(ano, turno, municipio, faixa_etaria) %>% 
-  summarise(aptos = sum(aptos),
-            abstencoes = sum(abstencoes))
+  summarize(aptos = sum(aptos),
+            abstencoes = sum(abstencoes)) %>%
+  mutate(
+    faixa_etaria = fct_collapse(
+      faixa_etaria,
+      "18 a 20 anos" = c("20 anos", "19 anos", "18 anos"),
+      "16 e 17 anos" = c("17 anos", "16 anos")),
+    faixa_etaria = fct_relevel(faixa_etaria, "100 anos ou mais", after = Inf))
+
+#Análise descritiva
+df %>% 
+  filter(faixa_etaria != "Inválido") %>% 
+  group_by(turno, faixa_etaria) %>% 
+  summarize(aptos = sum(aptos), abstencoes = sum(abstencoes)) %>% 
+  mutate(comparecimento = 1- abstencoes/aptos,
+         comparecimento = ifelse(turno == 1, -comparecimento, comparecimento)) %>% 
+  ggplot(aes(x = faixa_etaria, y = comparecimento, fill = factor(turno))) +
+  geom_col() +
+  # geom_hline(yintercept = -0.8371519, linetype = "dashed") +
+  # geom_hline(yintercept = 0.8470657, linetype = "dashed") +
+  coord_flip() +
+  labs(x = "", y = "Comparecimento", fill = "Turno") 
+
+ggsave("output/piramide.png", dpi = 600, height = 4, width = 5)
+
+write.csv(df, "output/data-2022.csv")
 
 df <- df %>% 
   mutate(tx_abstencao = abstencoes / aptos) %>% 
@@ -36,3 +60,6 @@ lm(`60 a 64 anos` ~ passe_livre + factor(uf), data = df) %>%
 
 lm(`55 a 59 anos` ~ passe_livre + factor(uf), data = df) %>% 
   summary(.)
+
+
+  
