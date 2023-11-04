7727d58affd6de3b0a023be61dc6c83f93874424
gustavo-tm <gtheilm@gmail.com>
2023-11-04 17:04:30 -0300

    download dados comparecimento

---
+++ download_comparecimento.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import requests
+import zipfile
+import pathlib
+import os
+import io
+
+## dicionario com os nomes dos arquivos e suas respectivas urls
+dados = {
+    f'perfil_comparecimento_abstencao_{ano}.csv': f'https://cdn.tse.jus.br/estatistica/sead/odsele/perfil_comparecimento_abstencao/perfil_comparecimento_abstencao_{ano}.zip' for ano in [2014, 2018, 2022]}
+
+dir = pathlib.Path.cwd() / 'dados/comparecimento'
+dir.mkdir(exist_ok=True, parents=True)
+
+for nome_arquivo, url in dados.items():
+    path_dados = dir / nome_arquivo
+
+    if not path_dados.exists():
+        print(f'Fazendo download de {nome_arquivo}')
+        response = requests.get(url)
+
+        with zipfile.ZipFile(io.BytesIO(response.content), 'r') as zip_ref:
+            with zip_ref.open(nome_arquivo) as csv_file:
+                (pd.read_csv(csv_file, encoding = "latin", sep = ";")
+                    .groupby(["ANO_ELEICAO", "NR_TURNO", "CD_MUNICIPIO", "DS_FAIXA_ETARIA"])
+                    .agg({col: "sum" for col in ["QT_APTOS", "QT_COMPARECIMENTO", "QT_ABSTENCAO"]})
+                    .reset_index()
+                    .to_csv(path_dados, index=False)
+                )
+
+        print(f'{nome_arquivo} foi salvo')
